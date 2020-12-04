94b67a73298680825c4d69a3d83102b16cb6fdd5
Dominic Frey <freydominic96@gmail.com>
2020-12-04 20:19:39 +0100

    Fixed GlobalStyles import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { addDecorator } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
-import  { theme, GlobalStyle } from '../src'
+import  { theme } from '../src'
+import GlobalStyle from '../src/theme/globalStyles'
 
 addDecorator((story) => (
   <>
